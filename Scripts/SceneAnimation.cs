24f2282db761c0cc4102bbb223942a2a84bf4cef
Trong Nguyen <nguyentrong101094@gmail.com>
2023-12-15 15:53:03 +0700

    Scene animation: add toggle to allow skip show & hide animation.

--- Scripts/SceneAnimation.cs
+++ Scripts/SceneAnimation.cs
@@ -11,28 +11,33 @@ namespace Omnilatent.ScenesManager
     public class SceneAnimation : MonoBehaviour
     {
         //[SerializeField] Controller m_Controller;
-#if OMNILATENT_SIMPLE_ANIMATION
-        [SerializeField] SimpleAnimObject animObject;
-#endif
+        #if OMNILATENT_SIMPLE_ANIMATION
+        [SerializeField] protected SimpleAnimObject animObject;
+        #endif
+        [Tooltip("If true, show animation will be skipped")] [SerializeField]
+        protected bool showInstantly = false;
+
+        [Tooltip("If true, hide animation will be skipped")] [SerializeField]
+        protected bool hideInstantly = false;
 
         //public Controller Controller { get => m_Controller; set => m_Controller = value; }
 
-        public void Show(Action onEndShow)
+        public virtual void Show(Action onEndShow)
         {
-#if OMNILATENT_SIMPLE_ANIMATION
-            animObject.Show(onEndShow);
-#else
+            #if OMNILATENT_SIMPLE_ANIMATION
+            animObject.Show(onEndShow, showInstantly);
+            #else
             onEndShow();
-#endif
+            #endif
         }
 
-        public void Hide(Action onEndHide)
+        public virtual void Hide(Action onEndHide)
         {
-#if OMNILATENT_SIMPLE_ANIMATION
-            animObject.Hide(onEndHide);
-#else
+            #if OMNILATENT_SIMPLE_ANIMATION
+            animObject.Hide(onEndHide, hideInstantly);
+            #else
             onEndHide();
-#endif
+            #endif
         }
     }
 }
